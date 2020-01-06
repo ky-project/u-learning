52e293a4f6e91bdd3aa65ed6f4819f13f31f764f
luyuhao <ludaye1112@163.com>
2020-01-06 18:48:37 +0800

    系统管理添加自定义时间转换器

--- ulearning-system-manage/src/main/java/com/ky/ulearning/system/common/config/WebMvcConfig.java
+++ ulearning-system-manage/src/main/java/com/ky/ulearning/system/common/config/WebMvcConfig.java
@@ -2,10 +2,12 @@ package com.ky.ulearning.system.common.config;
 
 import com.ky.ulearning.common.core.exceptions.handler.GlobalExceptionHandler;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 /**
  * WebMvc配置类
@@ -30,31 +32,4 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public GlobalExceptionHandler defaultGlobalExceptionHandler() {
         return new GlobalExceptionHandler();
     }
-//
-//    @Override
-//    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-//        configurer.defaultContentType(MediaType.APPLICATION_JSON_UTF8);
-//    }
-//
-//    /**
-//     * 规定SpringContext在String和Date时的用的转化器
-//     */
-//    @PostConstruct
-//    public void initEditableValidation() {
-//        ConfigurableWebBindingInitializer initializer = (ConfigurableWebBindingInitializer) handlerAdapter
-//                .getWebBindingInitializer();
-//        if (initializer != null && initializer.getConversionService() != null) {
-//            GenericConversionService genericConversionService = (GenericConversionService) initializer
-//                    .getConversionService();
-//            genericConversionService.addConverter(String.class, Date.class, new String2DateConverter());
-//        }
-//    }
-//
-//    private class String2DateConverter implements Converter<String, Date> {
-//        @SuppressWarnings("NullableProblems")
-//        @Override
-//        public Date convert(String source) {
-//            return DateUtil.parseDate(source);
-//        }
-//    }
 }
