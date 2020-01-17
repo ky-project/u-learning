08f224c11213e5caa2371c7406d3774f39855fd8
luyuhao <ludaye1112@163.com>
2020-01-17 20:23:08 +0800

    auto update

--- ulearning-system-manage/src/main/java/com/ky/ulearning/system/sys/controller/TeachingTaskController.java
+++ ulearning-system-manage/src/main/java/com/ky/ulearning/system/sys/controller/TeachingTaskController.java
@@ -4,16 +4,22 @@ import com.ky.ulearning.common.core.annotation.Log;
 import com.ky.ulearning.common.core.annotation.PermissionName;
 import com.ky.ulearning.common.core.message.JsonResult;
 import com.ky.ulearning.common.core.utils.ResponseEntityUtil;
+import com.ky.ulearning.common.core.utils.StringUtil;
 import com.ky.ulearning.common.core.utils.TermUtil;
+import com.ky.ulearning.common.core.validate.ValidatorBuilder;
 import com.ky.ulearning.spi.common.vo.TermVo;
+import com.ky.ulearning.spi.system.dto.TeachingTaskDto;
+import com.ky.ulearning.system.common.constants.SystemErrorCodeEnum;
 import com.ky.ulearning.system.common.constants.SystemManageConfigParameters;
 import com.ky.ulearning.system.sys.service.TeachingTaskService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiOperationSupport;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -48,5 +54,21 @@ public class TeachingTaskController {
         List<TermVo> termList = TermUtil.getTermList(preYears, nextYears);
 
         return ResponseEntityUtil.ok(JsonResult.buildData(termList));
+    }
+
+    @Log("添加教学任务")
+    @ApiOperation(value = "添加教学任务")
+    @ApiOperationSupport(ignoreParameters = {"id"})
+    @PermissionName(source = "course:save", name = "添加教学任务", group = "课程管理")
+    @PostMapping("/save")
+    public ResponseEntity save(TeachingTaskDto teachingTaskDto) {
+        ValidatorBuilder.build()
+                .on(StringUtil.isEmpty(teachingTaskDto.getTeaId()), SystemErrorCodeEnum.TEA_ID_CANNOT_BE_NULL)
+                .on(StringUtil.isEmpty(teachingTaskDto.getCourseId()), SystemErrorCodeEnum.COURSE_ID_CANNOT_BE_NULL)
+                .on(StringUtil.isEmpty(teachingTaskDto.getTeachingTaskAlias()), SystemErrorCodeEnum.TEACHING_TASK_ALIAS_CANNOT_BE_NULL)
+                .on(StringUtil.isEmpty(teachingTaskDto.getTerm()), SystemErrorCodeEnum.TERM_CANNOT_BE_NULL)
+                .doValidate().checkResult();
+
+        return ResponseEntityUtil.ok(JsonResult.buildMsg("添加成功"));
     }
 }
