f533f20f2cce4398da02e9d44a09bb74fb463559
luyuhao <ludaye1112@163.com>
2020-02-06 03:41:55 +0800

    新增文件记录表

---
+++ modules/database/sql/1.0.0/u_file_record.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS `u_file_record`;
+CREATE TABLE `u_file_record`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '文件记录ID',
+  `record_url` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件记录url',
+  `record_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件记录名',
+  `record_size` bigint(15) NULL DEFAULT NULL COMMENT '文件记录名大小',
+  `record_type` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件记录类型',
+  `valid` tinyint(1) NULL DEFAULT 1 COMMENT '是否有效',
+  `memo` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
+  `create_time` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `create_by` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建者',
+  `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
+  `update_by` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '更新者',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
