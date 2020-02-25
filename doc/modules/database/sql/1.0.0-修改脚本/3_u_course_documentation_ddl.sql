9d6ebdf46ef00d2da7ef780d401c706d162a5712
luyuhao <ludaye1112@163.com>
2020-02-25 22:33:22 +0800

    描述：【SQL】去除u_course_documentation表和u_course_resource表的标题列非空属性 日期：2020-02-25

--- doc/modules/database/sql/1.0.0-修改脚本/3_u_course_documentation_ddl.sql
+++ doc/modules/database/sql/1.0.0-修改脚本/3_u_course_documentation_ddl.sql
@@ -4,3 +4,5 @@ alter table u_course_documentation drop column documentation_URL;
 alter table u_course_documentation drop column documentation_attachment;
 
 ALTER TABLE u_course_documentation MODIFY `documentation_category` smallint(6) NOT NULL COMMENT '文件资料分类';
+
+ALTER TABLE u_course_documentation MODIFY `documentation_title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '标题';
--- doc/modules/database/sql/1.0.0-修改脚本/3_u_course_resource_ddl.sql
+++ doc/modules/database/sql/1.0.0-修改脚本/3_u_course_resource_ddl.sql
@@ -1,2 +1,4 @@
 -- 删除字段
 alter table u_course_resource drop column resource_URL;
+
+ALTER TABLE u_course_resource MODIFY `resource_title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '标题'
