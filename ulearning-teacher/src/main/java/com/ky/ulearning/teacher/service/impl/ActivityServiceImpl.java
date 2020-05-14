278f818f5fa8936fe692914c67e51726f54b2ed1
luyuhao <ludaye1112@163.com>
2020-05-14 10:02:50 +0800

    描述：【教师端】解决循环依赖 日期：2020-05-14

--- ulearning-teacher/src/main/java/com/ky/ulearning/teacher/service/impl/ActivityServiceImpl.java
+++ ulearning-teacher/src/main/java/com/ky/ulearning/teacher/service/impl/ActivityServiceImpl.java
@@ -20,6 +20,7 @@ import com.ky.ulearning.teacher.service.SendEmailService;
 import com.ky.ulearning.teacher.service.TeachingTaskExperimentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -69,6 +70,7 @@ public class ActivityServiceImpl extends BaseService implements ActivityService 
     @Autowired
     private SendEmailService sendEmailService;
 
+    @Lazy
     @Autowired
     private TeachingTaskExperimentService teachingTaskExperimentService;
 
--- ulearning-teacher/src/main/java/com/ky/ulearning/teacher/service/impl/TeachingTaskExperimentServiceImpl.java
+++ ulearning-teacher/src/main/java/com/ky/ulearning/teacher/service/impl/TeachingTaskExperimentServiceImpl.java
@@ -24,6 +24,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -46,6 +47,7 @@ public class TeachingTaskExperimentServiceImpl extends BaseService implements Te
     @Autowired
     private TeachingTaskDao teachingTaskDao;
 
+    @Lazy
     @Autowired
     private ActivityService activityService;
 
